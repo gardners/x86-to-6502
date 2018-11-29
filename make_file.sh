a653ca76a7efad47e30bfb477169640f4cf21ba7
Paul Gardner-Stephen <paul@servalproject.org>
2018-11-29 13:35:23 +1030

    use g++ to generate 386 assembly that has correct assembly format.

--- make_file.sh
+++ make_file.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 clang++ -std=c++1z -Wc++11-extensions -o x86-to-6502 src/main.cpp && \
-clang++ -std=c++1z -c -O3 -o- -Wall -Wextra -m32 -march=i386 -ggdb -S $1 > $1.x86.asm && \
+g++ -std=c++1z -c -O3 -o- -Wall -Wextra -m32 -march=i386 -S $1 > $1.x86.asm && \
 echo "===== 6502 ====="
 cat $1.x86.asm | ./x86-to-6502 -m 6502 > $1.6502.asm && \
 cat $1.6502.asm | sed -e "/^\t\..*$/d" > $1.asm && \
